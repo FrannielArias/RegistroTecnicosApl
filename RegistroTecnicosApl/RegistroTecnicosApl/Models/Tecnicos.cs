97bc118910db65f6a4667be5600f0f271af586af
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-08-28 23:27:39 -0400

    Creación de los modelos

---
+++ RegistroTecnicosApl/RegistroTecnicosApl/Models/Tecnicos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicosApl.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int EstudianteId {  get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "No se permite numeros en este campo.")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [Range(maximum:9999999999, minimum:0.1, ErrorMessage = "El monto debe ser mayor a 0 o es demasiado alto para el sistema")]
+        public decimal SueldoHora { get; set; }
+    }
+}
