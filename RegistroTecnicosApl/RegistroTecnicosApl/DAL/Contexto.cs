e7ea0aaa1fbe8e631789dad089e3c679c6d0b0ad
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-08-29 00:05:03 -0400

    Cree el contexto

---
+++ RegistroTecnicosApl/RegistroTecnicosApl/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicosApl.Models;
+
+namespace RegistroTecnicosApl.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) 
+            : base(options){ }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; }
+    }
+}
