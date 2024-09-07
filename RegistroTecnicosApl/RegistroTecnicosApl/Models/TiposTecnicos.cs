b4353d10f15a7694ed2fe90a84a9f65a7166278e
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-09-07 15:08:33 -0400

    Cree el modelo de tipoTecnico.

---
+++ RegistroTecnicosApl/RegistroTecnicosApl/Models/TiposTecnicos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO.Compression;
+
+namespace RegistroTecnicosApl.Models
+{
+    public class TiposTecnicos
+    {
+        [Key]
+        public int TipoTecnicoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten letras en este campo.")]
+        public string? Descripcion { get; set; }
+    }
+}
