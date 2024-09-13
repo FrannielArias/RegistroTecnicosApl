357ec3f3e34db7b38f37599374bbfff4bcbd6fd3
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-09-13 16:27:25 -0400

    Pasando el contexto de clientes

--- RegistroTecnicosApl/RegistroTecnicosApl/DAL/Contexto.cs
+++ RegistroTecnicosApl/RegistroTecnicosApl/DAL/Contexto.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnicosApl.Models;
+using RegistroTecnicosApl.Services;
 
 namespace RegistroTecnicosApl.DAL
 {
@@ -10,5 +11,9 @@ namespace RegistroTecnicosApl.DAL
 
         public DbSet<Tecnicos> Tecnicos { get; set; }
         public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+
+        public DbSet<Clientes> Clientes { get; set; }
+
+        public DbSet<Trabajos> Trabajos { get; set; }
     }
 }
