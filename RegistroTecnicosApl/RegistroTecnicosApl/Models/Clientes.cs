62bb4900adcf6716def7d121200f856d7eefcb74
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-09-13 16:26:34 -0400

    Creacion del modelo de clientes

---
+++ RegistroTecnicosApl/RegistroTecnicosApl/Models/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicosApl.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Solo se permiten letras en este campo.")]
+        public string? Nombres { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [StringLength(12, ErrorMessage = "El numero de WhatsApp solo debe tener 10 digitos")]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}", ErrorMessage = "El numero de WhatsApp debe tener este formato xxx-xxx-xxxx.")]
+        public string? WhatsApp { get; set; }
+    }
+}
