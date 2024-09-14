e29300f0a09b48f493c2129295e8d73bbd1cedbd
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-09-14 18:58:38 -0400

    Inyeccion del services de trabajo

--- RegistroTecnicosApl/RegistroTecnicosApl/Program.cs
+++ RegistroTecnicosApl/RegistroTecnicosApl/Program.cs
@@ -15,11 +15,12 @@ builder.Services.AddDbContext<Contexto>(t => t.UseSqlite(ConStr));
 builder.Services.AddScoped<TecnicoServices>();
 builder.Services.AddScoped<TiposTecnicosServices>();
 builder.Services.AddScoped<ClientesServices>();
+builder.Services.AddScoped<TrabajosServices>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())   
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
