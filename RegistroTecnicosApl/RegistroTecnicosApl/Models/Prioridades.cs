776c0fcfe469b591e6a9a713aad2e9fb4b85a449
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-09-21 18:49:41 -0400

    Creando el modelo de prioridad

---
+++ RegistroTecnicosApl/RegistroTecnicosApl/Models/Prioridades.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicosApl.Models
+{
+    public class Prioridades
+    {
+        [Key]
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Solo se permiten letras en este campo.")]
+        public string? Descripcion {get; set;}
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        public TimeSpan Tiempo {get; set;}
+
+    }
+}
