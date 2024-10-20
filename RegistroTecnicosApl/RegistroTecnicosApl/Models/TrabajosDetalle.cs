32db4d516117a9486975598905051eb1161c901e
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-10-20 00:43:59 -0400

    cree coneciones con articulos  trabajo

---
+++ RegistroTecnicosApl/RegistroTecnicosApl/Models/TrabajosDetalle.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicosApl.Models
+{
+    public class TrabajosDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public decimal Costo { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int ArticuloId { get; set; }
+        [ForeignKey("ArticuloId")]
+        public Articulos? Articulos { get; set; }
+
+        public int TrabajoId { get; set; }
+        [ForeignKey("TrabajoId")]
+        public Trabajos? Trabajos { get; set; }
+
+    }
+}
