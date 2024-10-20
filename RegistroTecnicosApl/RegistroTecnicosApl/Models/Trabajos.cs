9370d3d875bd7cf4fd78046d0bb779829b1b77ad
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-10-19 22:26:41 -0400

    Relacione el modelo de trabajo con detalle

--- RegistroTecnicosApl/RegistroTecnicosApl/Models/Trabajos.cs
+++ RegistroTecnicosApl/RegistroTecnicosApl/Models/Trabajos.cs
@@ -9,7 +9,7 @@ namespace RegistroTecnicosApl.Models
         [Key]
         public int TrabajoId { get; set; }
 
-        public DateTime? Fecha { get; set; }
+        public DateTime? Fecha { get; set; } = DateTime.Now;
 
         [Required(ErrorMessage = "Este campo es obligatorio.")]
         [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Solo se permiten letras.")]
@@ -19,17 +19,20 @@ namespace RegistroTecnicosApl.Models
         [Range(maximum: 99999999, minimum: 0.1, ErrorMessage = "No se permiten valores menores a 0.")]
         public decimal Monto { get; set; }
 
-        public int ClienteId {  get; set; }
+        public int ClienteId { get; set; }
         [ForeignKey("ClienteId")]
-        public Clientes? Cliente { get; set; }
+        public Clientes? Clientes { get; set; }
 
-        public int TecnicoId {  get; set; }
+        public int TecnicoId { get; set; }
         [ForeignKey("TecnicoId")]
-        public Tecnicos? tecnicos { get; set; }
+        public Tecnicos? Tecnicos { get; set; }
 
         public int PrioridadId { get; set; }
         [ForeignKey("PrioridadId")]
 
         public Prioridades? Prioridades { get; set; }
+
+        public ICollection<TrabajosDetalle> TrabajoDetalle { get; set; }
+
     }
 }
