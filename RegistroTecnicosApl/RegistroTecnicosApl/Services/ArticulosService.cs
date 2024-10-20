2e1c37a6c9b01833802c559bf4e98b4bc3e25450
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-10-20 00:44:11 -0400

    Cree el metodo listar

---
+++ RegistroTecnicosApl/RegistroTecnicosApl/Services/ArticulosService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicosApl.DAL;
+using RegistroTecnicosApl.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicosApl.Services;
+
+public class ArticulosService(Contexto _contexto)
+{
+    public async Task<List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
+    {
+        return await _contexto.Articulos
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
