6089e2e9a086cb6b0fea7e21bd1ad293a3df7d52
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2024-10-20 00:44:48 -0400

    inclui al trabajo detalle y los articulos

--- RegistroTecnicosApl/RegistroTecnicosApl/Services/TrabajosServices.cs
+++ RegistroTecnicosApl/RegistroTecnicosApl/Services/TrabajosServices.cs
@@ -15,18 +15,38 @@ public class TrabajosServices
     {
         _contexto = contexto;
     }
-
     public async Task<bool> Guardar(Trabajos trabajo)
     {
         if (!await Existe(trabajo.TrabajoId))
         {
+            foreach (var detalle in trabajo.TrabajoDetalle)
+            {
+                var articulo = await BuscarArticulos(detalle.ArticuloId);
+                if (articulo != null)
+                {
+                    articulo.Existencia -= detalle.Cantidad;
+                    await ActualizarArticulo(articulo);
+                }
+            }
             return await Insertar(trabajo);
+
         }
         else
-        {
             return await Modificar(trabajo);
-        }
     }
+
+    //public async Task<bool> Guardar(Trabajos trabajo)
+    //{
+    //    if (!await Existe(trabajo.TrabajoId))
+    //    {
+    //        return await Insertar(trabajo);
+    //    }
+    //    else
+    //    {
+    //        return await Modificar(trabajo);
+    //    }
+    //}
+
     private async Task<bool> Existe(int trabajoId)
     {
         return await _contexto.Trabajos
@@ -52,18 +72,23 @@ public class TrabajosServices
             .Where(t => t.TrabajoId == trabajo.TrabajoId)
             .ExecuteDeleteAsync() > 0;
     }
-        
+
     public async Task<Trabajos?> Buscar(int trabajoId)
     {
         return await _contexto.Trabajos
+            .Include(t => t.Tecnicos)
+            .Include(c => c.Clientes)
+            .Include(p => p.Prioridades)
             .AsNoTracking()
             .FirstOrDefaultAsync(t => t.TrabajoId == trabajoId);
     }
     public async Task<Trabajos?> BuscarTodo(int trabajoId)
     {
         return await _contexto.Trabajos
-            .Include(t => t.tecnicos)
-            .Include(c => c.Cliente)
+            .Include(t => t.TrabajoId)
+            .Include(c => c.Clientes)
+            .Include(p => p.Prioridades)
+            .Include(d => d.TrabajoDetalle)
             .AsNoTracking()
             .FirstOrDefaultAsync(t => t.TrabajoId == trabajoId);
     }
@@ -71,10 +96,48 @@ public class TrabajosServices
     public async Task<List<Trabajos>> Listar(Expression<Func<Trabajos, bool>> criterio)
     {
         return await _contexto.Trabajos
-            .Include(t => t.tecnicos)
-            .Include(c => c.Cliente)
+            .Include(d => d.TrabajoDetalle)
+            .Include(t => t.Tecnicos)
+            .Include(c => c.Clientes)
+            .Include(p => p.Prioridades)
             .AsNoTracking()
             .Where(criterio)
             .ToListAsync();
+    }
+
+    public async Task<List<Articulos>> ListarArticulos()
+    {
+        return await _contexto.Articulos
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<List<TrabajosDetalle>> ListarDetalle(int trabajoId)
+    {
+        var detalle = await _contexto.TrabajoDetalle
+            .Where(d => d.TrabajoId == trabajoId)
+            .ToListAsync();
+
+        return detalle;
+    }
+    public async Task<List<TrabajosDetalle>> BuscarDetalle(int id)
+    {
+        return await _contexto.TrabajoDetalle
+            .Include(td => td.Articulos)
+            .AsNoTracking()
+            .Where(td => td.TrabajoId == id)
+            .ToListAsync();
+    }
+    public async Task<Articulos> BuscarArticulos(int id)
+    {
+        return await _contexto.Articulos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.ArticuloId == id);
+    }
+    public async Task<bool> ActualizarArticulo(Articulos articulo)
+    {
+        _contexto.Articulos.Update(articulo);
+        return await _contexto
+            .SaveChangesAsync() > 0;
     }
 }
