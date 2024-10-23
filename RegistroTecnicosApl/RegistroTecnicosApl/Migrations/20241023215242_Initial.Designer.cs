2e417b733673e082790233910fe7a94f32e9b63c
FrannielArias <156683746+FrannielArias@users.noreply.github.com>
2024-10-23 18:30:36 -0400

    Agregando las notificaciones

--- RegistroTecnicosApl/RegistroTecnicosApl/Migrations/20241023215242_Initial.Designer.cs
+++ RegistroTecnicosApl/RegistroTecnicosApl/Migrations/20241023215242_Initial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroTecnicosApl.DAL;
 
@@ -11,9 +12,11 @@ using RegistroTecnicosApl.DAL;
 namespace RegistroTecnicosApl.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20241023215242_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RegistroTecnicosApl/RegistroTecnicosApl/Models/CotizacionesDetalle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicosApl.Models
+{
+    public class CotizacionesDetalle
+    {
+        [Key]
+        public int CotizacionDetalleId { get; set; }
+
+        [ForeignKey("CotizacionId")]
+        public int CotizacionId { get; set; }
+        public Cotizaciones Cotizaciones { get; set; }
+
+        [ForeignKey("ArticuloId")]
+        public int ArticuloId { get; set; }
+        public Articulos Articulos { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [Range(minimum: 0.1, maximum: 9999999999999, ErrorMessage = "El monto debe ser mayor a 0.")]
+        public double Cantidad { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [Range(minimum: 0.1, maximum: 9999999999999, ErrorMessage = "El monto debe ser mayor a 0.")]
+        public double Precio { get; set; }
+
+    }
+}
