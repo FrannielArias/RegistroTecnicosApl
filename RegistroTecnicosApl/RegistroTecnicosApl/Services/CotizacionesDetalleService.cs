7d39cb46262ca18662201762ce24583a5ccb5f22
FrannielArias <156683746+FrannielArias@users.noreply.github.com>
2024-10-26 20:03:27 -0400

    Cree los servicios de cotizacion

---
+++ RegistroTecnicosApl/RegistroTecnicosApl/Services/CotizacionesDetalleService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicosApl.DAL;
+using RegistroTecnicosApl.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicosApl.Services;
+
+public class CotizacionesDetalleService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<List<CotizacionesDetalle>> Listar(Expression<Func<CotizacionesDetalle, bool>> criterio)
+    {
+        await using var _contexto = await DbFactory.CreateDbContextAsync();
+
+        return await _contexto.CotizacionesDetalle
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+}
